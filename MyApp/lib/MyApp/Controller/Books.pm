8c6abd6016ccedc8b1f33f0b072b93a3c68b6828
Vinícius Schmidt <vinicius.schmidt@gmail.com>
2012-01-22 18:41:42 -0200

    adding url_create method

--- MyApp/lib/MyApp/Controller/Books.pm
+++ MyApp/lib/MyApp/Controller/Books.pm
@@ -54,6 +54,41 @@ sub list :Local {
 
 
 
+=head2 url_create
+    
+    Create a book with the supplied title, rating, and author
+    
+=cut
+    
+sub url_create :Local {
+        # In addition to self & context, get the title, rating, &
+        # author_id args from the URL.  Note that Catalyst automatically
+        # puts extra information after the "/<controller_name>/<action_name/"
+        # into @_.  The args are separated  by the '/' char on the URL.
+        my ($self, $c, $title, $rating, $author_id) = @_;
+    
+        # Call create() on the book model object. Pass the table
+        # columns/field values we want to set as hash values
+        my $book = $c->model('DB::Book')->create({
+                title  => $title,
+                rating => $rating
+            });
+    
+        # Add a record to the join table for this book, mapping to
+        # appropriate author
+        $book->add_to_book_authors({author_id => $author_id});
+        # Note: Above is a shortcut for this:
+        # $book->create_related('book_authors', {author_id => $author_id});
+    
+        # Assign the Book object to the stash for display and set template
+        $c->stash(book     => $book,
+                  template => 'books/create_done.tt2');
+    
+        # Disable caching for this page
+        $c->response->header('Cache-Control' => 'no-cache');
+}
+
+
 
 
 =head1 AUTHOR
