c26fdb15d79aae11a14ce36c4b6d75ca00c4cdcf
Vinnix Schmidt <vinnix.bsd@gmail.com>
2016-01-17 17:14:35 -0200

    Adding result of make

---
+++ FirstJQuery/blib/lib/FirstJQuery/View/JSON.pm
@@ -0,0 +1,29 @@
+package FirstJQuery::View::JSON;
+
+use strict;
+use base 'Catalyst::View::JSON';
+
+=head1 NAME
+
+FirstJQuery::View::JSON - Catalyst JSON View
+
+=head1 SYNOPSIS
+
+See L<FirstJQuery>
+
+=head1 DESCRIPTION
+
+Catalyst JSON View.
+
+=head1 AUTHOR
+
+Vinícius Abrahão Bazana Schmidt
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ FirstJQuery/blib/lib/FirstJQuery/View/TT.pm
@@ -0,0 +1,36 @@
+package FirstJQuery::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+);
+
+=head1 NAME
+
+FirstJQuery::View::TT - TT View for FirstJQuery
+
+=head1 DESCRIPTION
+
+TT View for FirstJQuery.
+
+=head1 SEE ALSO
+
+L<FirstJQuery>
+
+=head1 AUTHOR
+
+Vinícius Abrahão Bazana Schmidt
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
